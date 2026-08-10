4894a1107c94e04c254fe635122a3d5d075299ac
João Antonio Guilherme Melquiades Pereira <joaopmel1910@gmail.com>
2026-08-10 11:56:16 -0300

    Exercício 4 - estruturas de repetição e de dados

---
+++ exercicios/exercicio04.php
@@ -0,0 +1,66 @@
+<?php
+$linguagensDeProgramacao = [
+    [
+        "id" => 1,
+        "linguagem" => "HTML",
+        "descricao" => "Estruturação"
+    ],
+    [
+        "id" => 2,
+        "linguagem" => "CSS",
+        "descricao" => "Estilos"
+    ],
+    [
+        "id" => 3,
+        "linguagem" => "JS",
+        "descricao" => "Comportamentos"
+    ],
+    [
+        "id" => 4,
+        "linguagem" => "PHP",
+        "descricao" => "Back-End"
+    ],
+    [
+        "id" => 5,
+        "linguagem" => "SQL",
+        "descricao" => "Manipulação de Dados"
+    ]
+];
+?>
+
+
+
+
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercício 04: estruturas de repetição (loops) e estruturas de dados</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+</head>
+
+<body>
+
+
+    <?php
+    foreach ($linguagensDeProgramacao as $linguagens):
+    ?>
+        <ul>
+        <li>ID <?=$linguagens["id"]?>
+            linguagem <?=$linguagens["linguagem"] ?>
+            Descrição <?=$linguagens["descricao"] ?>
+    <?php
+    endforeach;
+    ?>
+    
+
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+</body>
+
+</html>
