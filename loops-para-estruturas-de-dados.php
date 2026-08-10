c27f5b3e9359715346036206345c106d6caa9a70
João Antonio Guilherme Melquiades Pereira <joaopmel1910@gmail.com>
2026-08-10 10:57:40 -0300

    Update loops-para-estruturas-de-dados.php

--- loops-para-estruturas-de-dados.php
+++ loops-para-estruturas-de-dados.php
@@ -5,6 +5,28 @@ $meses = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho"];
 
 ?>
 
+<?php
+    $planoDeEstudos = [ 
+    ["JS Avançado", "Node.js", "Next.js"],
+    ["PHP", "Orientação a Objetos"],
+    ["Teoria das Cores", "Photoshop com IA", "UX/UI",]
+    ];
+    $linhas = count($planoDeEstudos);
+?>
+
+<?php 
+    $alunos = ["Zidane", "Iniesta", "Busquets", "Renato Augusto" ];
+
+?>
+
+<?php 
+    $curso = [
+    "titulo" => "Analista de desempenho e performance",
+    "carga_horario" => "2000",
+    "descricao" => "Aprender tudo sobre a área"
+    ];
+?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
 
@@ -25,13 +47,73 @@ $meses = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho"];
 
         <h2>Usando o loop <code>for</code> para acessar o array</h2>
         <ol>
-            <?php for ($i = 0; $i < count($meses); $i++): ?>
+            <?php 
+                for ($i = 0; $i < count($meses); $i++): ?>
                 <li> <?= $meses[$i] ?> </li>
-            <?php endfor; ?>
+            <?php 
+                endfor; ?>
 
         </ol>
+        <hr><br>
+        <h2>Usando o loop <code>for</code> para acessar uma matriz (array de arrays)</h2>
 
+           <?php 
+                for ($k = 0; $k < $linhas; $k++): 
+                    $colunas = count($planoDeEstudos[$k]); //Acessa cada linha
+                    
+                     for ($j = 0; $j < $colunas; $j++): //Acessa cada coluna
+            ?>
 
+                <p> <?= $planoDeEstudos[$k][$j] ?> </p>
+
+            <?php  
+                    endfor;
+                endfor;
+            ?>
+                <hr><br>
+                <h2>Usando o loop <code>foreach</code> para arrays</h2>
+
+                <?php 
+                    foreach ($alunos as $aluno):             
+                ?>
+                    <p><?= $aluno?></p>
+                <?php
+                    endforeach;
+                ?>
+                <hr><br>
+                <h2>Usando <code>foreach</code> para array associativo</h2>
+                <?php 
+                    foreach ($curso as $chave => $valor):                
+                ?>
+                <p><b><?=$chave?></b> : <?=$valor?></p>
+                <?php 
+                endforeach;
+
+                // Extraindo somente o valor
+                foreach ($curso as $valor):
+                ?>
+                <hr>
+                <p><i><?=$valor?></i></p>
+
+                <?php 
+                endforeach;
+                 ?>
+
+                <hr><br>
+                <h2>Usando o <code>foreach</code> em uma matriz</h2>
+                <?php
+                    foreach($planoDeEstudos as $categoria): // Cada Linha
+                        foreach ($categoria as $assunto): 
+
+                ?>
+                <p><?=$assunto?></p>
+
+                <?php 
+                        endforeach;
+                    endforeach;                
+                ?>
+
+        
 
     </div>
 
