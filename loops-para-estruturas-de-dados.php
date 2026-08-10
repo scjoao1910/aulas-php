8bf7fed2014af6c5b63ab4727f85ce2b1776243f
João Antonio Guilherme Melquiades Pereira <joaopmel1910@gmail.com>
2026-08-10 10:00:43 -0300

    Usando array (Vetor)

---
+++ loops-para-estruturas-de-dados.php
@@ -0,0 +1,46 @@
+<?php
+//  Array (vetor)
+
+$meses = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho"];
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP - Loops para estruturas</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+
+</head>
+
+<body>
+    <div class="container">
+
+        <h1>Loops para estruturas de dados</h1>
+        <hr>
+
+        <h2>Usando o loop <code>for</code> para acessar o array</h2>
+        <ol>
+            <?php for ($i = 0; $i < count($meses); $i++): ?>
+                <li> <?= $meses[$i] ?> </li>
+            <?php endfor; ?>
+
+        </ol>
+
+
+
+    </div>
+
+
+
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+</body>
+
+</html>
