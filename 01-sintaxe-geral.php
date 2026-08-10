d69c8c85cc73dd539bbc8006eeb5eea5840b4e22
João Antonio Guilherme Melquiades Pereira <joaopmel1910@gmail.com>
2026-08-10 09:24:40 -0300

    Aplicando Booststrap em todos os documentos e recuperando alguns arquivos perdidos

--- 01-sintaxe-geral.php
+++ 01-sintaxe-geral.php
@@ -4,6 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP - Sintaxe Geral</title>
+
+    <!-- O nosso CSS/Estilo próprio sempre vir por último -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
     
     <!-- CSS INTERNO -->
     <style>
@@ -43,6 +46,8 @@ echo "<p><abbr title='HyperText Preprocessor'>PHP</abbr> é uma linguagem popula
 echo "<p><abbr title=\"HyperText Preprocessor\">PHP</abbr> é uma linguagem popular</p>";
 
 ?>
+<!-- Para o JS também, o nosso sempre tem que vir por último -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
 
 <script>
 
