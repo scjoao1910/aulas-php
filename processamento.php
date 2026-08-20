006461ff2ee95c5322ec31b636b2d4fe4b8ca7b6
João Antonio Guilherme Melquiades Pereira <joaopmel1910@gmail.com>
2026-08-20 11:51:43 -0300

    ...

---
+++ processamento.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+ 
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Processos PHP</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet"integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container">
+        <h1>Recebimento e processamento dos dados</h1>
+        <hr>
+        <?php
+        // CAPTURANDO OS DADOS DE CADA CAMPO
+        $nome = $_POST["nome"];
+        $email = $_POST["email"];
+        $idade = $_POST["idade"];
+        $mensagem = $_POST["mensagem"];
+        $interesses = $_POST["interesses"] ?? [];
+        $informativos = $_POST["informativos"];
+
+        /* Operador ?? -> coalescência nula
+        Caso nenhum interesse seja selecionado, a variável guardará um array vazio */
+        $interesses = $_POST["interesses"] ?? [];
+
+// Caso nenhuma opção seja selecionada, o valor "nao" fica como pa
+$informativos = $_POST["informativos"] ?? "nao";
+?>
+    <h2>Dados recebidos</h2>
+    <p>Nome: <?= $nome ?></p>
+    <p>E-mail: <?= $email ?></p>
+    <p>Idade: <?= $idade ?> anos</p>
+    <p>Mensagem: <?= $mensagem ?> </p>
+
+    <?php if(!empty($interesses)): ?>
+    <p>Interesses: <?= implode(", ", $interesses) ?></p>
+    <?php endif; ?>
+
+    <p>Informativos: 
+        <?= $informativos === "sim" ? "Sim" : "Não" ?>
+    </p>
+</div>
+ 
+   
+ 
+ 
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI"
+crossorigin="anonymous"></script>
+</body>
+ 
+</html>
+ 
