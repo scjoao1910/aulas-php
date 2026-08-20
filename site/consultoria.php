cb5537bad84a10566a4c210af0a4061d317cc14c
João Antonio Guilherme Melquiades Pereira <joaopmel1910@gmail.com>
2026-08-20 08:47:21 -0300

    Funciona Booststrap

---
+++ site/consultoria.php
@@ -0,0 +1,7 @@
+<?php require "includes/cabecalho.php" ?>
+    
+    <h2>Página consultoria</h2>
+    <p>Nossos profissionais estão a disposição</p>
+
+<?php require "includes/rodape.php" ?>
+    
---
+++ site/index.php
@@ -0,0 +1,7 @@
+<?php require "includes/cabecalho.php" ?>
+    
+    <h2>Bem-vindo ao site de exemplo</h2>
+    <p>Esta é a primeira página do nosso site</p>
+
+<?php require "includes/rodape.php" ?>
+    
