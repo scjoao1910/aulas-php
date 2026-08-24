54c1430221fe871084cbc5d20116b59026dea727
João Antonio Guilherme Melquiades Pereira <joaopmel1910@gmail.com>
2026-08-24 10:47:27 -0300

    ...

--- processamento.php
+++ processamento.php
@@ -10,49 +10,97 @@
 <div class="container">
     <h1>Recebimento e processamento dos dados</h1>
     <hr>
-
-<?php 
+ 
+<?php
 /* $_POST e $_GET
 Arrays superglobais que possuem os dados
 enviados a partir de formulários e/ou
 links dinâmicos. */
-
-// Lista de possíveis erros econtrados ao longo do processamento
-$erros = []; 
-
-
+ 
+// Lista de possiveis erros encontrados ao longo do preocessamento
+$erros =[];
+ 
 // Verificando se houve uma requisição POST
 if( $_SERVER["REQUEST_METHOD"] === "POST" ){
-
-    // Capturando os dados de cada campo
+ 
+    // Capturando e sanitizando/limpando os dados de cada campo
     $nome = filter_input(INPUT_POST, 'nome', FILTER_SANITIZE_SPECIAL_CHARS);
     $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
     $idade = filter_input(INPUT_POST, 'idade', FILTER_SANITIZE_NUMBER_INT);
     $mensagem = filter_input(INPUT_POST, 'mensagem', FILTER_SANITIZE_SPECIAL_CHARS);
-
-    /* Operador ?? -> coalescência nula
-    Caso nenhum interesse seja selecionado, 
-    a variável guardará um array vazio */
-    $interesses = $_POST["interesses"] ?? []; 
-
+ 
+    // Interesses
+    $interessesValidos = ["html", "css", "javascript"];
+ 
+    $interesses =
+    filter_input(INPUT_POST, 'interesses', FILTER_SANITIZE_SPECIAL_CHARS, FILTER_REQUIRE_ARRAY)?? [];
+   
+    if(!is_array($interesses)){
+        // Garatindo que ao menos vire um array vazio
+        $interesses = [];
+ 
+        // Registrando uma mensagem de erro no array de erros
+        $erros[] = "Seleção inválida de interesses";
+    }
+ 
+    // Comparar os dois arrays (o do formulário e o válidos) checando se os valores "batem"
+    $interessesValidados = array_intersect($interesses, $interessesValidos);
+   
+ 
     // Caso nenhuma opção seja selecionada, o valor "nao" fica como padrão
-    $informativos = $_POST["informativos"] ?? "nao";
-?>   
+
+    // Filtramos a entrada que o usuário escolheu
+    $opcoesValidas = ["sim", "nao"];
+    $informativos = filter_input(INPUT_POST, 'informativos', FILTER_SANITIZE_SPECIAL_CHARS);
+
+    // Verificamos se a escolha do usuário é uma das válidas. Se sim, usamos ela. Senão, usamos "não"
+    $informativos = in_array($informativos, $opcoesValidas) ? $informativos : "nao";
+    
+    //Validações (Campos obrigatórios)
+    if(empty($nome))$erros[] = "O campo nome é obrigatório";
+    if(empty($email))$erros[] = "O e-mail deve ser infomado";
+    if(empty($mensagem))$erros[] = "Você deve escrever uma mensagem";
+
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) $erros[] = "O e-mail não é válido";
+    
+    $filtroIdade = [
+        "options" =>[
+            "min_range" => 0,
+            "max_range" => 130,
+        ]
+    ];
+
+
+    if(!filter_var($idade, FILTER_VALIDATE_INT))
+        $erros[] = "Idade inválida. A idade deve ser estar entre 0 e 130";
+    if(!empty($erros)):
+?>  
+    <div class="alert alert-danger">
+        <h2>Erros encontrados:</h2>
+        <ul class="mb-3">
+            <?php foreach($erros as $erro): ?>
+                <li> <?= $erro ?></li>
+            <?php endforeach ?>    
+        </ul>
+        <a href="17-formulario.html" class="btn btn-warning">Voltar para o formulário!</a>
+    </div>
+<?php else: ?>
     <h2>Dados recebidos</h2>
     <p>Nome: <?= $nome ?></p>
     <p>E-mail: <?= $email ?></p>
     <p>Idade: <?= $idade ?> anos</p>
     <p>Mensagem: <?= $mensagem ?> </p>
-    
-    <?php if(!empty($interesses)): ?>
-    <p>Interesses: <?= implode(", ", $interesses) ?></p>
+   
+    <?php if(!empty($interessesValidados)): ?>
+    <p>Interesses: <?= implode(", ", $interessesValidados) ?></p>
     <?php endif; ?>
-
-    <p>Informativos: 
+ 
+    <p>Informativos:
         <?= $informativos === 'sim' ? "Sim" : "Não" ?>
     </p>
-    
+   
 <?php
+    endif;
 } else {
 ?>
     <!-- Acesso inválido (usuário não veio do formulário) -->
@@ -65,9 +113,9 @@ if( $_SERVER["REQUEST_METHOD"] === "POST" ){
 <?php  
 }
 ?>
-
+ 
 </div>
-
+ 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
 </body>
 </html>
